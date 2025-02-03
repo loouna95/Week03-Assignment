6f93284388181bbeec5705a6761d2fd049e49d5a
jappe <jappe@LOOUNA>
2025-02-03 11:38:49 -0800

    Add project files.

---
+++ Week03 Assignment/GiftCardAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class GiftCardAccount : BankAccount
+{
+    // GiftCardAccountConstruction
+    private readonly decimal _monthlyDeposit = 0m;
+
+    public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance)
+        => _monthlyDeposit = monthlyDeposit;
+
+    // AddMonthlyDeposit
+    public override void PerformMonthEndTransactions()
+    {
+        if (_monthlyDeposit != 0)
+        {
+            MakeDeposit(_monthlyDeposit, DateTime.Now, "Add monthly deposit");
+        }
+    }
+}
---
+++ Week03 Assignment/InterstEarningAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class InterestEarningAccount : BankAccount
+{
+    // DerivedConstructor
+    public InterestEarningAccount(string name, decimal initialBalance) : base(name, initialBalance)
+    {
+    }
+
+
+    // ApplyMonthendInterest
+    public override void PerformMonthEndTransactions()
+    {
+        if (Balance > 500m)
+        {
+            decimal interest = Balance * 0.02m;
+            MakeDeposit(interest, DateTime.Now, "apply monthly interest");
+        }
+    }
+}
